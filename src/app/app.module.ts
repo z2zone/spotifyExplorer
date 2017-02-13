71b8aee2a216d009e9bcf3c9decde8a85074c403
Brandon Cho <hjun1114@gmail.com>
2017-02-12 22:00:05 -0500

    added background and navbar template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
 
+import { AppComponent }   from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent , NavbarComponent ],
+  declarations: [ AppComponent , NavbarComponent , AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }﻿
